af1f028ca077abaf444d278757923b6e944e2a77
Hana Ali <hana142@live.com>
2021-05-18 11:03:06 +0400

    Removed illustrator gallery

--- src/components/mainPage/LandingItems/navbar/MenuItems.js
+++ src/components/mainPage/LandingItems/navbar/MenuItems.js
@@ -1,6 +1,5 @@
 const navbarLinks = [
     { title: 'Home Page', path: '/' },
-    { title: 'Illustrator Gallery', path: '/illustrator-gallery' },
     { title: 'Art Gallery', path: '/art-gallery' },
     { title: 'Challenges', path: '/challenges' }
 ];
