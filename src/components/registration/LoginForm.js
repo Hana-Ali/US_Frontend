afbe8f1eba4b95d68a5c4d542f09a983231d7d25
Hana Ali <hana142@live.com>
2021-05-21 17:37:20 +0400

    Experimenting with login route

--- src/components/registration/LoginForm.js
+++ src/components/registration/LoginForm.js
@@ -47,7 +47,7 @@ function LoginForm(props)
             formData.append('userName', userNameField.value);
             formData.append('password', passwordField.value);
 
-            fetch(`${process.env.REACT_APP_API_ENDPOINT}/user/login`, {
+            fetch(`${process.env.REACT_APP_API_ENDPOINT}user/login`, {
                 method: 'POST',
                 // headers: {"Content-Type": "application/json"},
                 body: formData
